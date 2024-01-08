3e5e53e122d43c0e75bbfda18ffcbfb0e3ffce82
Oscar Lee <qwas074506@yahoo.com>
2024-01-08 17:19:26 +0800

    add main code

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import random
+
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+a = 1
+b = 2
+
+print(add(a, b))
+print(minus(a, b))
+print(multiply(a, b))
+print(divide(a, b))
